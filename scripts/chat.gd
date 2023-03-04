94324b38fafc0cf794fdf710dcf72e6fbaa11d99
CDcruzCode <88635443+CDcruzCode@users.noreply.github.com>
2023-03-04 22:09:47 +1000

    Add files via upload

--- scripts/chat.gd
+++ scripts/chat.gd
@@ -10,14 +10,22 @@ var openai
 
 @onready var config_popup = $config_popup
 @onready var config_button = $vbox/PanelContainer/hbox2/config
+@onready var api_key_input = $config_popup/vbox/api_key
 @onready var prompt_options = $config_popup/vbox/prompt_options
-@onready var max_tokens = $config_popup/vbox/max_tokens
+@onready var max_tokens_input = $config_popup/vbox/max_tokens
 @onready var temperature_text = $config_popup/vbox/temperature_text
-@onready var temperature = $config_popup/vbox/temperature
+@onready var temperature_slider = $config_popup/vbox/temperature
 @onready var presence_text = $config_popup/vbox/presence_text
-@onready var presence_penalty = $config_popup/vbox/presence_penalty
+@onready var presence_penalty_slider = $config_popup/vbox/presence_penalty
 @onready var frequency_text = $config_popup/vbox/frequency_text
-@onready var frequency_penalty = $config_popup/vbox/frequency_penalty
+@onready var frequency_penalty_slider = $config_popup/vbox/frequency_penalty
+
+var config = ConfigFile.new()
+var API_KEY:String
+var MAX_TOKENS:int = 1000
+var TEMPERATURE:float = 1.4
+var PRESENCE:float = 0.4
+var FREQUENCY:float = 1.0
 
 var bot_thinking:bool = false
 var chat_memory:PackedStringArray = []
@@ -26,28 +34,38 @@ var bot_color:Color = Color("F5515F")
 var user_color:Color = Color("5D8EAC")
 
 
-var prompt_types:PackedStringArray = [
-	"general_prompt",
-	"proofreader"
-]
+var prompt_types:PackedStringArray = []
 
 func _ready():
-	await get_tree().process_frame
-	openai = await OpenAIAPI.new(get_tree(), "https://api.openai.com/v1/chat/", load_file_as_string("res://API_KEY.txt"))
-	
-	print("start")
-	openai.connect("request_success", _on_openai_request_success)
-	openai.connect("request_error", _on_openai_request_error)
-	
 	user_input.gui_input.connect(user_gui)
 	loading.hide()
 	config_popup.hide()
 	
 	config_button.pressed.connect(func(): config_popup.popup_centered())
-	prompt_options.item_selected.connect(func(): chat_label.text = "Chat Type: " + prompt_options.get_item_text(prompt_options.selected))
+	config_popup.confirmed.connect(save_config)
+	
+	prompt_types = list_folders_in_directory("res://scripts/prompts/")
+	
 	for p in prompt_types:
 		prompt_options.add_item(p)
-	prompt_options.select(0)
+	
+	prompt_options.item_selected.connect(func(_i): chat_label.text = "Chat Type: " + prompt_options.get_item_text(prompt_options.selected))
+	chat_label.text = "Chat Type: " + prompt_options.get_item_text(prompt_options.selected)
+	
+	
+	if(!load_config()):
+		config_popup.popup_centered()
+		return
+	
+	connect_openai()
+
+func connect_openai():
+	await get_tree().process_frame
+	openai = OpenAIAPI.new(get_tree(), "https://api.openai.com/v1/chat/", API_KEY)
+	#print("openai connected")
+	openai.connect("request_success", _on_openai_request_success)
+	openai.connect("request_error", _on_openai_request_error)
+
 
 func _on_openai_request_success(data):
 	print("Request succeeded:", data)
@@ -67,9 +85,57 @@ func _on_openai_request_success(data):
 	bot_thinking = false
 	chat_memory.append(reply)
 
-func _on_openai_request_error(error_code):
-	print("Request failed with error code:", error_code)
+func load_config():
+	
+	var err = config.load("user://settings.cfg")
+	if err != OK:
+		return false
+	
+	API_KEY = config.get_value("Settings", "API_KEY")
+	MAX_TOKENS = config.get_value("Settings", "MAX_TOKENS")
+	TEMPERATURE = config.get_value("Settings", "TEMPERATURE")
+	PRESENCE = config.get_value("Settings", "PRESENCE")
+	FREQUENCY = config.get_value("Settings", "FREQUENCY")
+	
+	api_key_input.text = API_KEY
+	max_tokens_input.value = MAX_TOKENS
+	temperature_slider.value = TEMPERATURE
+	presence_penalty_slider.value = PRESENCE
+	frequency_penalty_slider.value = FREQUENCY
+	print("config loaded")
+	return true
 
+func save_config():
+	config.set_value("Settings", "API_KEY", api_key_input.text)
+	config.set_value("Settings", "MAX_TOKENS", max_tokens_input.value)
+	config.set_value("Settings", "TEMPERATURE", temperature_slider.value)
+	config.set_value("Settings", "PRESENCE", presence_penalty_slider.value)
+	config.set_value("Settings", "FREQUENCY", frequency_penalty_slider.value)
+	config.save("user://settings.cfg")
+	
+	if(openai == null):
+		API_KEY = api_key_input.text
+		MAX_TOKENS = max_tokens_input.value
+		TEMPERATURE = temperature_slider.value
+		PRESENCE = presence_penalty_slider.value
+		FREQUENCY = frequency_penalty_slider.value
+		connect_openai()
+
+
+
+func _on_openai_request_error(error_code):
+	printerr("Request failed with error code:", error_code)
+	bot_thinking = false
+	var new_msg:PanelContainer = message_box.instantiate()
+	new_msg.get_node("message_box").size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
+	new_msg.get_node("message_box/msg").text = "An error occurred. Error Code: " + str(error_code)
+	new_msg.get_node("message_box").self_modulate = Color.DARK_RED
+	chat_log.add_child(new_msg)
+	loading.hide()
+	await get_tree().process_frame
+	chat_scroll.scroll_vertical = chat_scroll.get_v_scroll_bar().max_value
+
+#On ENTER pressed, send message instead of adding a new line.
 func user_gui(event:InputEvent):
 	if event is InputEventKey:
 		if event.keycode == KEY_ENTER && event.is_pressed() && !bot_thinking:
@@ -83,7 +149,7 @@ func send_message(msg:String, role:String = "user", model:String = "gpt-3.5-turb
 	loading.show()
 	print("USER MSG: "+msg)
 	
-	chat_memory.append("<USER> "+msg)
+	chat_memory.append("\n<USER> "+msg)
 	
 	
 	var new_msg = message_box.instantiate()
@@ -93,7 +159,7 @@ func send_message(msg:String, role:String = "user", model:String = "gpt-3.5-turb
 	chat_log.add_child(new_msg)
 	
 	
-	var pre_msg = load_prompt(prompt_options.get_item_text(prompt_options.selected))
+	var pre_msg = load_file_as_string("res://scripts/prompts/" + prompt_options.get_item_text(prompt_options.selected))
 	for m in chat_memory:
 		pre_msg += m + "\n"
 	
@@ -101,26 +167,18 @@ func send_message(msg:String, role:String = "user", model:String = "gpt-3.5-turb
 	var data = {
 	"model": model,
 	"messages": [{"role": role, "content": pre_msg}],
-	"temperature": 1.0,
-	"presence_penalty": 0.4,
-	"frequency_penalty": 1.0,
+	"max_tokens": MAX_TOKENS,
+	"temperature": TEMPERATURE,
+	"presence_penalty": PRESENCE,
+	"frequency_penalty": FREQUENCY,
 	"stream": false
 	}
+	print(data)
 	print(pre_msg)
 	openai.make_request("completions", HTTPClient.METHOD_POST, data)
-
-
-
-func load_prompt(prompt):
-	var file = FileAccess.open("res://scripts/prompts/" + prompt + ".txt", FileAccess.READ)
-	var content : String
-	if file != null:
-		content = file.get_as_text()
-		file = null
-		return content
-	else:
-		return "error"
-		#return "[load_file_as_string] FILE DID NOT OPEN"
+	
+	await get_tree().process_frame
+	chat_scroll.scroll_vertical = chat_scroll.get_v_scroll_bar().max_value
 
 
 func load_file_as_string(path):
@@ -133,3 +191,17 @@ func load_file_as_string(path):
 	else:
 		return "error"
 		#return "[load_file_as_string] FILE DID NOT OPEN"
+
+func list_folders_in_directory(path):
+	var files:PackedStringArray = []
+	var dir = DirAccess.open(path)
+	if dir != null:
+		dir.list_dir_begin()
+		while true:
+			var file = dir.get_next()
+			if file == "":
+				break
+			elif !file.begins_with("."):
+				files.append(file)
+		dir.list_dir_end()
+	return files
